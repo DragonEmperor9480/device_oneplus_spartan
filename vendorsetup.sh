2c5697be01ce56e221fc5b3b7692dc23b2c3a5db
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-09-06 09:08:05 +0530

    spartan: vendorsetup: add kernel path

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,5 @@ git clone https://github.com/DragonEmperor9480/oneplus_hardware_oplus hardware/o
 git clone https://github.com/provasish/proprietary_vendor_oneplus_sm8250-common.git vendor/oneplus/sm8250-common
 
 git clone https://github.com/provasish/proprietary_vendor_oneplus_spartan.git vendor/oneplus/spartan
+
+git clone https://github.com/provasish/android_kernel_oneplus_sm8250.git kernel/oneplus/sm8250
