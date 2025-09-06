42f4b738dac150393e14f9de2905fc8d23800d60
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-09-06 09:01:31 +0530

    spartan: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/DragonEmperor9480/android_device_oneplus_sm8250-common.git device/oneplus/sm8250-common -b infinity
+git clone https://github.com/DragonEmperor9480/device_oneplus_sm8250-common device/oneplus/sm8250-common -b infinity
 
-git clone https://github.com/provasish/hardware_oplus.git hardware/oplus
+git clone https://github.com/DragonEmperor9480/oneplus_hardware_oplus hardware/oplus
 
 git clone https://github.com/provasish/proprietary_vendor_oneplus_sm8250-common.git vendor/oneplus/sm8250-common
 
