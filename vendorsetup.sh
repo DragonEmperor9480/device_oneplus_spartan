f4d6db502b66959bc92ce658d1a12240190d93ab
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2026-01-04 23:41:08 +0530

    spartan: vendorsetup: Update vendor clone command to use lineage-23.1 branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ git clone https://github.com/DragonEmperor9480/device_oneplus_sm8250-common devi
 
 git clone https://github.com/DragonEmperor9480/oneplus_hardware_oplus hardware/oplus
 
-git clone https://github.com/provasish/proprietary_vendor_oneplus_sm8250-common.git vendor/oneplus/sm8250-common
+git clone https://github.com/provasish/proprietary_vendor_oneplus_sm8250-common.git vendor/oneplus/sm8250-common -b lineage-23.1
 
 git clone https://github.com/provasish/proprietary_vendor_oneplus_spartan.git vendor/oneplus/spartan
 
